8d3f304eb0a98ec3159f9fbaf6981b1a356ce55e
Aline096 <anoclez@gmail.com>
2025-04-11 12:55:16 +0200

    Add pagination on news page

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -42,6 +42,20 @@ const News = () => {
 
     fetchData()
   }, [])
+  const itemsPerPage = 9
+
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const sortedBlogs = blogs
+    ?.slice()
+    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+
+  const totalPages = Math.ceil(sortedBlogs.length / itemsPerPage)
+
+  const currentBlogs = sortedBlogs.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  )
 
   if (error) {
     console.log(error)
@@ -58,54 +72,85 @@ const News = () => {
         <div className="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-sm"></div>
       </div>
       <div className="container mx-auto px-4 py-12">
-        <h1 className="text-4xl font-bold mb-16 text-center">What&apos;s new ?</h1>
+        <h1 className="text-4xl font-bold mb-16 text-center">
+          What&apos;s new ?
+        </h1>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-          {blogs
-            ?.slice()
-            .sort(
-              (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
-            )
-            .map((blog) => (
-              <div
-                key={blog._id}
-                className="group bg-white rounded-2xl shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-300 relative"
-              >
-                <div className="overflow-hidden">
-                  <Image
-                    width={500}
-                    height={300}
-                    src={blog.picture}
-                    alt={blog.title}
-                    className="w-full h-56 object-cover transform group-hover:scale-105 transition-transform duration-300"
-                  />
+          {currentBlogs.map((blog) => (
+            <div
+              key={blog._id}
+              className="group bg-white rounded-2xl shadow-md overflow-hidden hover:shadow-xl transition-shadow duration-300 relative"
+            >
+              <div className="overflow-hidden">
+                <Image
+                  width={500}
+                  height={300}
+                  src={blog.picture}
+                  alt={blog.title}
+                  className="w-full h-56 object-cover transform group-hover:scale-105 transition-transform duration-300"
+                />
+              </div>
+
+              <div className="p-5">
+                <div className="flex justify-between text-sm text-gray-500 mb-2">
+                  <span>{new Date(blog.date).toLocaleDateString()}</span>
+                  <span>{blog.location}</span>
                 </div>
+                <h2 className="text-xl font-semibold text-gray-800 group-hover:text-blue-600 transition-colors duration-300">
+                  {blog.title}
+                </h2>
+                <p className="mt-2 text-gray-600 text-sm leading-relaxed line-clamp-3">
+                  {/* {blog.description} */}
+                  {truncateText(blog.description, 150)}
+                </p>
 
-                <div className="p-5">
-                  <div className="flex justify-between text-sm text-gray-500 mb-2">
-                    <span>{new Date(blog.date).toLocaleDateString()}</span>
-                    <span>{blog.location}</span>
-                  </div>
-                  <h2 className="text-xl font-semibold text-gray-800 group-hover:text-blue-600 transition-colors duration-300">
-                    {blog.title}
-                  </h2>
-                  <p className="mt-2 text-gray-600 text-sm leading-relaxed line-clamp-3">
-                    {/* {blog.description} */}
-                    {truncateText(blog.description, 150)}
-                  </p>
-
-                  <div className="mt-4">
-                    <a
-                      href={`/blog/${blog._id}`}
-                      className="text-sm text-blue-500 hover:text-blue-700 font-medium"
-                    >
-                      Read more →
-                    </a>
-                  </div>
+                <div className="mt-4">
+                  <a
+                    href={`/news/${blog._id}`}
+                    className="text-sm text-blue-500 hover:text-blue-700 font-medium"
+                  >
+                    Read more →
+                  </a>
                 </div>
               </div>
-            ))}
+            </div>
+          ))}
         </div>
+        {blogs.length !== 0 &&
+          <div className="flex justify-center mt-8 space-x-2">
+            <button
+              disabled={currentPage === 1}
+              onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+              className="px-4 py-2 text-sm border rounded disabled:opacity-50"
+            >
+              Previous
+            </button>
+
+            {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+              <button
+                key={page}
+                onClick={() => setCurrentPage(page)}
+                className={`px-4 py-2 text-sm border rounded ${page === currentPage
+                    ? 'bg-blue-500 text-white'
+                    : 'hover:bg-gray-200'
+                  }`}
+              >
+                {page}
+              </button>
+            ))}
+
+            <button
+              disabled={currentPage === totalPages}
+              onClick={() =>
+                setCurrentPage((prev) => Math.min(prev + 1, totalPages))
+              }
+              className="px-4 py-2 text-sm border rounded disabled:opacity-50"
+            >
+              Next
+            </button>
+          </div>
+        }
       </div>
     </div>
   )
